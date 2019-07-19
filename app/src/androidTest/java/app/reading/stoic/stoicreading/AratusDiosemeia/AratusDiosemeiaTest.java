938246224f3579f0af47cbf5e2bb0e582c4c8a2d
zikalify <zikalify@gmail.com>
2019-07-19 23:35:21 +0100

    Practising unit testing because I'm useless at it, hopefully I'll make them do something useful ;-)

---
+++ app/src/androidTest/java/app/reading/stoic/stoicreading/AratusDiosemeia/AratusDiosemeiaTest.java
@@ -0,0 +1,44 @@
+package app.reading.stoic.stoicreading.AratusDiosemeia;
+
+import android.support.test.rule.ActivityTestRule;
+import android.view.View;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import app.reading.stoic.stoicreading.R;
+
+import static org.junit.Assert.*;
+
+public class AratusDiosemeiaTest {
+
+    @Rule
+    public ActivityTestRule<AratusDiosemeia> AratusDiosemeiaTestRule = new ActivityTestRule<AratusDiosemeia>(AratusDiosemeia.class);
+
+    private AratusDiosemeia AratusDiosemeia = null;
+
+    @Before
+    public void setUp() throws Exception {
+
+        AratusDiosemeia = AratusDiosemeiaTestRule.getActivity();
+
+    }
+
+    @Test
+    public void testLaunch(){
+
+        View view = AratusDiosemeia.findViewById(R.id.textView2);
+
+        assertNotNull(view);
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+        AratusDiosemeia = null;
+
+    }
+}

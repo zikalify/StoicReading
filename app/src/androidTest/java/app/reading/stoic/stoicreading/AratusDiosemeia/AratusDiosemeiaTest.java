0078f5fa14e29fd3bcb8cf2382886e5ca66581f6
zikalify <zikalify@gmail.com>
2019-07-20 10:05:03 +0100

    Added a new unit test to check that the correct title is produced.

--- app/src/androidTest/java/app/reading/stoic/stoicreading/AratusDiosemeia/AratusDiosemeiaTest.java
+++ app/src/androidTest/java/app/reading/stoic/stoicreading/AratusDiosemeia/AratusDiosemeiaTest.java
@@ -1,5 +1,7 @@
 package app.reading.stoic.stoicreading.AratusDiosemeia;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.support.test.rule.ActivityTestRule;
 import android.view.View;
 
@@ -21,24 +23,24 @@ public class AratusDiosemeiaTest {
 
     @Before
     public void setUp() throws Exception {
-
         AratusDiosemeia = AratusDiosemeiaTestRule.getActivity();
-
     }
 
     @Test
     public void testLaunch(){
-
         View view = AratusDiosemeia.findViewById(R.id.textView2);
-
         assertNotNull(view);
+    }
 
+    @Test
+    public void testTitle(){
+        String title = "Diosemeia";
+        String string1 = AratusDiosemeia.getResources().getString(R.string.AratusDiosemeiaTitle);
+        assertEquals(title, string1);
     }
 
     @After
     public void tearDown() throws Exception {
-
         AratusDiosemeia = null;
-
     }
 }
